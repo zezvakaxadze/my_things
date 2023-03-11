96bc31808a109e33950e5193e0e9269420cd86d4
zezvakaxadze <omniatv2000@gmail.com>
2023-03-11 20:36:11 +0300

    modification of odd and even app

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,19 +1,21 @@
-#odd or even app
+""" odd or even app """
+
 while True:
-    n = input("write number and i will tell you it's odd or even, for exit write exit: ")
-    if n.upper() == "EXIT":
-        break
-    elif n[0] == "-" and n[1:]:
-        if int(n) % 2 == 1:
-            print(f"{n} is odd")
-        elif int(n) % 2 == 0:
-            print(f"{n} is even")
-    elif n.isdigit():
-        if int(n) % 2 == 1:
-            print(f"{n} is odd")
-        elif int(n) % 2 == 0:
-            print(f"{n} is even")
-    elif float(n):
-        print("write integer number!")
-    else:
-        print("write number! ")
+    try:
+        n = input("write number and i will tell you it's odd or even, for exit write exit: ")
+        if n.upper() == "EXIT":
+            break
+        elif n[0] == "-" and int(n[1:]):
+            if int(n) % 2 == 1:
+                print(f"{n} is odd")
+            elif int(n) % 2 == 0:
+                print(f"{n} is even")
+        elif n.isdigit():
+            if int(n) % 2 == 1:
+                print(f"{n} is odd")
+            elif int(n) % 2 == 0:
+                print(f"{n} is even")
+        elif float(n):
+            print("write integer number!")
+    except ValueError:
+        print("write number!!!")
