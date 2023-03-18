54469da0ce89ee1d80ae10995086bba249008f6b
zezvakaxadze <omniatv2000@gmail.com>
2023-03-18 15:32:49 +0300

    just task

---
+++ positiv_number.py
@@ -0,0 +1,8 @@
+# if only one number is a negative number print False
+# in another situations print True
+
+a = input("write number: ")
+b = input("write number: ")
+
+print((a.isdigit and a[0] != '-' and b[0] == "-" and b[1:].isdigit()) or (a[0] == '-' and b.isdigit() and b[0] != "-"
+                                                                          and a[1:].isdigit()))
