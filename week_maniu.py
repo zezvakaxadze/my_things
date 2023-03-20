5c6c8db1f455d6fc8b24f0556a706f4706c84f42
zezvakaxadze <omniatv2000@gmail.com>
2023-03-20 08:57:08 +0400

    modification

--- week_maniu.py
+++ week_maniu.py
@@ -12,16 +12,10 @@ top_10_dishes = ["ბელგიური ვაფლი ნაყინით
                  "პასტა",
                  ]
 
-seven_days = []
-
-
 for i in range(7):
     c = random.choice(top_10_dishes)
-    seven_days.append(c)
     top_10_dishes.remove(c)
-
-for i in range(7):
-    print(f"დღე {i + 1}# {seven_days[i]}")
+    print(f"დღე {i + 1}# {c}")
 
 
 
